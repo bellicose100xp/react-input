63756a6109dc3cfe985ccd5a3ad6389c1a145f72
bellicose <bellicose.agnostic@gmail.com>
2015-10-02 23:50:47 -0700

    firebase start

--- src/flux/customerStore.js
+++ src/flux/customerStore.js
@@ -17,7 +17,7 @@ var ref = new Firebase("https://buggy-react.firebaseio.com/");
 let addCustomer = (newCustomer) => {
     ref.push(newCustomer);
 };
-// test
+// test 1
 
 let customerStore = Object.assign({}, EventEmitter.prototype, {
 
